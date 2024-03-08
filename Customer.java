2196b82ceef4fa77882f57cc57b6b320b59a60b1
Arthur B <72992146+Atlas002@users.noreply.github.com>
2024-03-08 14:40:59 +0000

    Update Customer.java
    changed constructor to be aligned with superclass constructor

--- Customer.java
+++ Customer.java
@@ -1,14 +1,11 @@
 public class Customer extends Person {
     private String address;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
     private String phoneNumber;
 
     public Customer(String address,  String firstName, String lastName, String email, String password,    String phoneNumber) {
-        super(name, email, phoneNumber);
+        super(firstName, lastName, email, password);
         this.address = address;
+        this.phoneNumber = phoneNumber;
     }
 
     // getters and setters
