6c72cf7b0a979f3fec2aac4231020fde4f98dbfc
Arthur B <72992146+Atlas002@users.noreply.github.com>
2024-03-08 14:28:49 +0000

    Update Booking.java
    added duration of stay to a booking (used to calculate booking price)

--- Booking.java
+++ Booking.java
@@ -5,12 +5,14 @@ public class Booking {
     private Room room;
     private LocalDate checkInDate;
     private LocalDate checkOutDate;
+    private int rentDuration;
 
-    public Booking(Customer customer, Room room, LocalDate checkInDate, LocalDate checkOutDate) {
+    public Booking(Customer customer, Room room, LocalDate checkInDate, LocalDate checkOutDate, int rentDuration) {
         this.customer = customer;
         this.room = room;
         this.checkInDate = checkInDate;
         this.checkOutDate = checkOutDate;
+        this.rentDuration = rentDuration;
     }
 
 
