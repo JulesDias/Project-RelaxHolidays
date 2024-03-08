96b4e8cce66fda7c688c96e43d3ebe0cb944f82f
Arthur B <72992146+Atlas002@users.noreply.github.com>
2024-03-08 14:32:19 +0000

    Update Customer.java
    updatated parameters

--- Customer.java
+++ Customer.java
@@ -1,7 +1,12 @@
 public class Customer extends Person {
     private String address;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phoneNumber;
 
-    public Customer(String name, String email, String phoneNumber, String address) {
+    public Customer(String address,  String firstName, String lastName, String email, String password,    String phoneNumber) {
         super(name, email, phoneNumber);
         this.address = address;
     }
