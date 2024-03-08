8acefc47bec4524efc2b8a362e4f63d4bd7282f4
Arthur B <72992146+Atlas002@users.noreply.github.com>
2024-03-08 15:43:33 +0000

    Update Room.java
    added variables to provide more flexible use of class

--- Room.java
+++ Room.java
@@ -2,10 +2,16 @@ public abstract class Room {
     private int roomNumber;
     private double price;
     private boolean isBooked;
+    private int adultCap;
+    private int childCap;
+    private int nbRooms;
 
-    public Room(int roomNumber, double price) {
+    public Room(int roomNumber, double price, int adultCap, int childCap, int nbRooms) {
         this.roomNumber = roomNumber;
         this.price = price;
+        this.adultCap = adultCap;
+        this.childCap = childCap;
+        this.nbRooms = nbRooms;
         this.isBooked = false;
     }
 
